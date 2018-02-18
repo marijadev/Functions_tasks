555cb48b6ee14d7482cd7484c2e285d00f42c9c6
marijadev <marija.fo@gmail.com>
2018-02-18 18:27:11 +0100

    add functions1, stars task

--- functions/functions1/function5.js
+++ functions/functions1/function5.js
@@ -5,17 +5,31 @@
 // *    *
 // *****
 
-function squareDraw() {
+function squareDraw(input) {
     var asterisk = "*";
     var blank = " ";
     var row = "\n";
     var result = "";
 
-    for(var i = 0; i < 5; i++) {
-        for(var j = 0; j < 5; j++) {
-            
+    for (var i = 0; i < input; i++) {
+        if (i == 0 || i == input - 1) {
+            for (var j = 0; j < input; j++) {
+                result += asterisk;
+            }
+        } else {
+            for (var j = 0; j < input; j++) {
+                if (j == 0 || j == input - 1) {
+                    result += asterisk;
+                } else {
+                    result += blank;
+                }
+            }
         }
+        result += row;
     }
+
+    console.log(result);
+    return result;
 }
 
-console.log(squareDraw());
+squareDraw(15);
